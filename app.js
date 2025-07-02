5cf0716bcdf2cbc9687d73c24e60b51e0aec1078
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-02 15:44:18 +0100

    Finished Auth0 quickstart tutorial

--- app.js
+++ app.js
@@ -1,50 +1,39 @@
+const { auth } = require('express-openid-connect');
 const express = require('express');
 const dotenv = require('dotenv')
-const passport = require('passport');
-const Auth0Strategy = require('passport-auth0');
-const session = require('express-session');
-
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
 
-const strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK_URL,
-  },
-  (accessToken, refreshToken, extraParams, profile, done) => {
-    return done(null, profile);
-  }
-);
-passport.use(strategy);
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+const config = {
+  authRequired: false, 
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUERBASEURL
+};
 
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
+// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-    res.send('Welcome to Node.js Auth0 Authenticaton!');
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
-app.get(
-  '/login',
-  passport.authenticate('auth0', {
-    scope: 'openid email profile',
-  })
-);
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(req.oidc.user);
+});
+
+app.get('/toad', requiresAuth(), (req, res) => {
+  res.send('helloa');
+});
+
 
 
 app.listen(PORT, () => {
